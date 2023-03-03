1a866b368094b286d8e524c1bb0da59f035a16d8
galah92 <galaharoni92@gmail.com>
2023-03-03 22:38:40 +0000

    Added lapin and made a connection to RabbitMQ

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,76 @@
 use axum::{response::Html, routing::get, Router};
+use lapin::{
+    message::DeliveryResult,
+    options::{BasicAckOptions, BasicConsumeOptions, BasicPublishOptions, QueueDeclareOptions},
+    types::FieldTable,
+    BasicProperties, Connection, ConnectionProperties,
+};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
-    // build our application with a route
+    let uri = "amqp://rabbitmq";
+    let options = ConnectionProperties::default()
+        .with_executor(tokio_executor_trait::Tokio::current())
+        .with_reactor(tokio_reactor_trait::Tokio);
+
+    let connection = Connection::connect(uri, options).await.unwrap();
+    let channel = connection.create_channel().await.unwrap();
+
+    let _queue = channel
+        .queue_declare(
+            "queue_test",
+            QueueDeclareOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+
+    let consumer = channel
+        .basic_consume(
+            "queue_test",
+            "tag_foo",
+            BasicConsumeOptions::default(),
+            FieldTable::default(),
+        )
+        .await
+        .unwrap();
+
+    consumer.set_delegate(move |delivery: DeliveryResult| async move {
+        let delivery = match delivery {
+            Ok(Some(delivery)) => delivery,
+            Ok(None) => return,
+            Err(error) => {
+                dbg!("Failed to consume queue message {}", error);
+                return;
+            }
+        };
+
+        println!("{:#?}", delivery.data);
+
+        delivery
+            .ack(BasicAckOptions::default())
+            .await
+            .expect("Failed to ack send_webhook_event message");
+    });
+
+    channel
+        .basic_publish(
+            "",
+            "queue_test",
+            BasicPublishOptions::default(),
+            b"Hello world!",
+            BasicProperties::default(),
+        )
+        .await
+        .unwrap()
+        .await
+        .unwrap();
+
     let app = Router::new()
         .route("/", get(handler))
         .route("/healthcheck", get(healthcheck));
 
-    // run it
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {addr}");
     axum::Server::bind(&addr)
