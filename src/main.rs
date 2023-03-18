86a8925e7fdd6b4129328d85b28acba579e9635d
galah92 <galaharoni92@gmail.com>
2023-03-18 09:06:20 -0400

    Added SharedState

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
 use axum::{extract::State, response::Html, routing::get, Router};
-use std::{
-    net::SocketAddr,
-    sync::{Arc, Mutex},
-};
+use std::{collections::HashMap, net::SocketAddr, sync::Arc};
+use tokio::sync::RwLock;
 use tokio_stream::StreamExt;
 use yamq::Broker;
 
@@ -11,22 +9,23 @@ async fn main() {
     let broker = Broker::bind("127.0.0.1:1883").await;
     let mut subcriber = broker.subscription("#").await.unwrap();
 
-    let count = Arc::new(Mutex::new(0));
+    let shared_state = Arc::new(RwLock::new(AppState::default()));
 
-    let subscriber_count = count.clone();
+    let subscriber_state = shared_state.clone();
     tokio::spawn(async move {
         loop {
-            let _message = subcriber.next().await;
-            let mut count = subscriber_count.lock().unwrap();
-            *count += 1;
+            let msg = subcriber.next().await.unwrap().unwrap();
+            let topic = msg.topic.as_ref().to_string();
+            let payload = String::from_utf8(msg.payload.to_vec()).unwrap();
+            subscriber_state.write().await.db.insert(topic, payload);
         }
     });
 
     let app = Router::new()
         .route("/", get(handler))
         .route("/healthcheck", get(healthcheck))
-        .route("/count", get(display_count))
-        .with_state(count.clone());
+        .route("/state", get(display_state))
+        .with_state(shared_state.clone());
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {addr}");
@@ -42,7 +41,15 @@ async fn handler() -> Html<&'static str> {
 
 async fn healthcheck() {}
 
-async fn display_count(State(count): State<Arc<Mutex<i32>>>) -> String {
-    let count = count.lock().unwrap();
-    format!("The count is {count}")
+async fn display_state(State(state): State<SharedState>) -> String {
+    let db = &state.read().await.db;
+    let data = format!("{:?}", db);
+    data
+}
+
+type SharedState = Arc<RwLock<AppState>>;
+
+#[derive(Default)]
+struct AppState {
+    db: HashMap<String, String>,
 }
